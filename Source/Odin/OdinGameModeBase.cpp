2a1f23636f77e57fbc7b59d7f6782757523a1e70
James <jwdeharopwd@gmail.com>
2023-10-05 20:04:01 +0200

    Deleted Zelda project parts

--- Source/Odin/OdinGameModeBase.cpp
+++ Source/Odin/OdinGameModeBase.cpp
@@ -1,21 +1,11 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-
 #include "OdinGameModeBase.h"
-
-#include "Actors/OD_BasePlayerState.h"
-#include "Libraries/OD_NetLibrary.h"
 
 void AOdinGameModeBase::PostLogin(APlayerController* NewPlayer)
 {
-	if (UOD_NetLibrary::IsServer(this))
-	{
-		FString s;
-	}
-	AOD_BasePlayerState* PlayerState = NewPlayer ? NewPlayer->GetPlayerState<AOD_BasePlayerState>() : nullptr;
-	if (PlayerState)
-	{
-		PlayerState->Initialize();
-	}
+	//AOD_BasePlayerState* PlayerState = NewPlayer ? NewPlayer->GetPlayerState<AOD_BasePlayerState>() : nullptr;
+	//if (PlayerState)
+	//{
+	//	PlayerState->Initialize();
+	//}
 	Super::PostLogin(NewPlayer);
 }
